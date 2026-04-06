f942bf4494c093cfe76eb1649734c2456a3bae9a
Pratham0514 <prathameshroundale05@gmail.com>
2026-04-06 17:47:58 +0530

    setup of the server side

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        if(conn){
+            console.log("MongoDB connected successfully 📦✅");
+        }
+    }catch(error){
+        console.error("Error connecting to MongoDB: 📦 ❌", error);
+    }
+};
+connectDB();
+{/* Health check */}
+app.get('/', (req, res) => {
+    res.status(200).json({ status: 'ok' ,
+        message: 'Welcome to the PBlogs API!' });
+});
+   
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
