7cf854107e701fdc478cc797ff6e229cdd215f43
Pratham0514 <prathameshroundale05@gmail.com>
2026-04-09 17:45:21 +0530

    add - client routing setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import AllBlogs from "./views/AllBlogs.jsx";
+import EditBlog from "./views/EditBlog.jsx";
+import NewBlog from "./views/NewBlog.jsx";
+import ReadBlog from "./views/ReadBlog.jsx";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
+
+createRoot(document.getElementById('root')).render(  
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<AllBlogs />} />
+            <Route path='/edit/:id' element={<EditBlog />} />
+            <Route path='/new' element={<NewBlog />} />
+            <Route path='/read/:slug' element={<ReadBlog />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='*' element={<h1>404 Not Found</h1>} />
+        </Routes>
+    </BrowserRouter>
+  
 )
