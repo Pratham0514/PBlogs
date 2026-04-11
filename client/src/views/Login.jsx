2953132b8be512caf8967b64811ac87ca0bbb100
Pratham0514 <prathameshroundale05@gmail.com>
2026-04-11 19:28:38 +0530

    add- login and signup parat done

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
-
+import { Link } from 'react-router'
+import { useState } from 'react'
+import axois from 'axios'
 function Login() {
+  const [user ,setUser] = useState({
+    email:"",
+    password:""
+   })
+
+   const loginUser = async()=>{
+    try{
+      const response = await axois.post(`${import.meta.env.VITE_API_URL}/login`,user);
+      console.log(response.data);
+    }
+    catch(error){
+      console.log(error)
+    }
+   }
+
   return (
-    <div>Login</div>
+    <div className="min-h-screen flex items-center justify-center bg-[#F5EFE6] px-4">
+  
+  <div className="bg-[#CBDCEB] w-full max-w-md p-6 sm:p-8 rounded-2xl shadow-lg">
+    
+    <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6 text-gray-700">
+      Login
+    </h1>
+
+    <div className="flex flex-col gap-4">
+  
+      <input
+        type="email"
+        placeholder="Email"
+        className="p-3 sm:p-4 rounded-lg bg-[#E8DFCA] outline-none focus:ring-2 focus:ring-[#6D94C5] text-sm sm:text-base"
+        value={user.email}
+        onChange={(e)=> setUser({...user , email: e.target.value})}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        className="p-3 sm:p-4 rounded-lg bg-[#E8DFCA] outline-none focus:ring-2 focus:ring-[#6D94C5] text-sm sm:text-base"
+        value={user.password}
+         onChange={(e)=> setUser({...user , password: e.target.value})}
+      />
+
+      <button className="bg-[#6D94C5] text-white p-3 sm:p-4 rounded-lg font-semibold hover:opacity-90 transition text-sm sm:text-base" onClick={()=> loginUser()}>
+        Login
+      </button>
+
+      <p className="text-xl text-center text-gray-600 mt-4">
+        Don't have an account?{' '}
+        <Link to="/signup" className="text-[#6D94C5] font-semibold hover:underline">
+          Signup
+        </Link>
+      </p>
+    </div>
+
+  </div>
+</div>
   )
 }
 
--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
-
+import { Link } from 'react-router'
+import { useState } from 'react'
+import axois from 'axios'
 function Signup() {
+
+ const [user ,setUser] = useState({
+  name:"",
+  email:"",
+  password:""
+ })
+
+ const signupUser = async()=>{
+  try{
+    const response = await axois.post(`${import.meta.env.VITE_API_URL}/signup`,user);
+    console.log(response.data);
+  }
+  catch(error){
+    console.log(error)
+  }
+ }
   return (
-    <div>Signup</div>
+   <div className="min-h-screen flex items-center justify-center bg-[#F5EFE6] px-4">
+  
+  <div className="bg-[#CBDCEB] w-full max-w-md p-6 sm:p-8 rounded-2xl shadow-lg">
+    
+    <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6 text-gray-700">
+      Signup
+    </h1>
+
+    <div className="flex flex-col gap-4">
+      
+      <input
+        type="text"
+        placeholder="Name"
+        className="p-3 sm:p-4 rounded-lg bg-[#E8DFCA] outline-none focus:ring-2 focus:ring-[#6D94C5] text-sm sm:text-base"
+        value={user.name}
+        onChange={(e)=> setUser({...user ,name:e.target.value})}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        className="p-3 sm:p-4 rounded-lg bg-[#E8DFCA] outline-none focus:ring-2 focus:ring-[#6D94C5] text-sm sm:text-base"
+        value={user.email}
+        onChange={(e)=> setUser({...user , email: e.target.value})}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        className="p-3 sm:p-4 rounded-lg bg-[#E8DFCA] outline-none focus:ring-2 focus:ring-[#6D94C5] text-sm sm:text-base"
+        value={user.password}
+         onChange={(e)=> setUser({...user , password: e.target.value})}
+      />
+
+      <button className="bg-[#6D94C5] text-white p-3 sm:p-4 rounded-lg font-semibold hover:opacity-90 transition text-sm sm:text-base" onClick={signupUser}>
+        Signup
+      </button>
+      <p className="text-xl text-center text-gray-600 mt-4">
+        Already have an account?{' '}
+        <Link to="/login" className="text-[#6D94C5] font-semibold hover:underline">
+          Login
+        </Link>
+      </p>
+    </div>
+
+  </div>
+</div>
   )
 }
 
